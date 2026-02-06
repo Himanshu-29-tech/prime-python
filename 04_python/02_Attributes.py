409f199972db74d87965df3c0196d483da1e2878
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-02-06 01:09:18 +0000

    practice problem

--- 04_python/02_Attributes.py
+++ 04_python/02_Attributes.py
@@ -72,26 +72,25 @@ class laptop:
 
 #example
 
-class laptop:
-        storage_type = "ssd"
+class Laptop:
+    storage_type = "ssd"   # class variable
+
+    def __init__(self, ram, storage):
+        self.ram = ram
+        self.storage = storage
+
+    @classmethod
+    def get_storage_type(cls):
+        print(f"storage type = {cls.storage_type}")
+
+    def get_info(self):   # instance method
+        print(f"Laptop has {self.ram} RAM & {self.storage} {self.storage_type}")
 
 
-def __init__(self,Ram,storage):
-    self.Ram = Ram
-    self.storage = storage
-
-@classmethod
-def get_storage_type(cls):
-    print(f"storage type = {cls.storage_type}")
-
-def get_info(self): #instance method
-    print(F"laptop has{self.Ram} Ram & {self.storage} {self.storage_type}")
-
-
-l1 = laptop("16gb" , "512gb")
+l1 = Laptop("16GB", "512GB")
 
 l1.get_storage_type()
-
+l1.get_info()
 
 
 #================ Static ==============
@@ -101,26 +100,53 @@ l1.get_storage_type()
 # decorator @staticmethod --->>> 
 
 
-class laptop:
-        storage_type = "ssd"
+class Laptop:
+    storage_type = "ssd"
+
+    def __init__(self, ram, storage):
+        self.ram = ram
+        self.storage = storage
+
+    @classmethod
+    def get_storage_type(cls):
+        print(f"storage type = {cls.storage_type}")
+
+    def get_info(self):  # instance method
+        print(f"Laptop has {self.ram} RAM & {self.storage} {self.storage_type}")
+
+    @staticmethod
+    def calc_discount(price, discount):
+        final_price = price - (discount * price / 100)
+        print(f"Discounted price = {final_price}")
 
 
-def __init__(self,Ram,storage):
-    self.Ram = Ram
-    self.storage = storage
+l1 = Laptop("16GB", "512GB")
 
-@classmethod
-def get_storage_type(cls):
-    print(f"storage type = {cls.storage_type}")
+l1.get_storage_type()
+l1.get_info()
+l1.calc_discount(40_000, 10)
 
-def get_info(self): #instance method
-    print(f"laptop has{self.Ram} Ram & {self.storage} {self.storage_type}")
 
-@staticmethod
-def clc_discount(price,discount):
-    final_price = price - (discount * price / 100)
-    print(f"discounted price = {final_price}")
 
-l1 = laptop("16gb" , "512gb")
 
-l1.calc_discount(40_000,10)
+#======== practice problem ===============
+
+# Product store
+# Design & create an online store for Products (name, price).
+# Track total products being created.
+# Create a static method to calculate discount on each product based on a % parameter.
+
+
+class product:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+
+    def get_info(self):
+        print(f"price of {self.name} is Rs.{self.price}")
+
+p1 = product("phone", 10_000)
+p2 = product("laptop", 50_000)
+p3 = product("pen", 10)
+
+p1.get_info()
