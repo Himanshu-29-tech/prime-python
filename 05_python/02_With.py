49b08f7199a3d3f16480c6e019666a8a74e206cd
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-02-08 17:25:37 +0000

    with & delete

---
+++ 05_python/02_With.py
@@ -0,0 +1,15 @@
+#With keyword
+
+""" with open("file_name","r") as f:
+    print9(f.read())
+"""
+with open("sample.txt", "r") as f:
+    data = f.read()
+    print(len(data))
+
+
+#delete files
+
+# import os
+
+# os.remove("sample.txt")
