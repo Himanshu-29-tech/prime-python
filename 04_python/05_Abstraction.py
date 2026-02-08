e3088759ebb0f312be8d71eda32f91d0cbf17ede
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-02-08 06:10:49 +0000

    Abstraction

--- 04_python/05_Abstraction.py
+++ 04_python/05_Abstraction.py
@@ -1,4 +1,26 @@
 #============ Abstraction =============
-#Hiding internal details & showing onlyessential features
- # absrtract class --0>> blureprint of otgher class
+#Hiding internal details & showing only essential features
+#Absrtract class --0>> blureprint of other class
+
+from abc import ABC, abstractmethod #(abstraction based classes)
+#Abstract class ->> that's willl not be performed
+class Animal(ABC):
+    def make_sound():
+        pass
+
+
+        
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar!")
+class Cow(Animal):
+    def make_sound(self):
+        print("Moo!")
+
+lion = Lion()
+lion.make_sound()
+
+cow = Cow()
+cow.make_sound()
+
  
